4ba5c4ac4f35cb9bc5596f327568cbe455f38e84
eazaykova <lena.zaykova.95@gmail.com>
2023-07-04 17:10:33 +0700

    The initial project settings have been completed. Added .env, .eslintrc.json, .gitignore, .prettierrc, jsconfig.json, package.json, src/, webpack.config.js and yarn.lock.

---
+++ src/core/router/routes.data.js
@@ -0,0 +1,12 @@
+import { Home } from '@/components/screens/home/home.component';
+
+export const ROUTES = [
+	{
+		path: '/',
+		component: Home
+	}
+	/*{
+		path: '/auth',
+		component: Auth
+	}*/
+];
